a29ae20ff6d1c8fc0b9fb8432df103a4523df788
Otis <otis@azblink.com>
2016-11-16 14:06:19 +0800

    Initial Commit

---
+++ VSCinema/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VSCinema
+//
+//  Created by Mac mini on 2016/11/16.
+//  Copyright © 2016年 Mac mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
